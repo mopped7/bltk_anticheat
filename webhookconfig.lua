9af1da086113ab1f7486dc9fd42fd1970d5235e3
Bence <66963670+Blitoka33@users.noreply.github.com>
2022-01-31 15:47:07 +0200

    Update webhookconfig.lua

--- webhookconfig.lua
+++ webhookconfig.lua
@@ -1,5 +1,5 @@
 webhooks = {} -- DON'T TOUCH
 
-webhooks.StatusHook = "https://canary.discord.com/api/webhooks/920398150424997959/QaKGwhSUJ1mDWnN3cvaY2mr-MsQ0_RgUaANlr8ypoRif-tp81swaTKiyuyrvRCwdUn44" -- Status Webhook (WORK IN PROGRESS)
-webhooks.ScreenShotStorage = "https://canary.discord.com/api/webhooks/920398150424997959/QaKGwhSUJ1mDWnN3cvaY2mr-MsQ0_RgUaANlr8ypoRif-tp81swaTKiyuyrvRCwdUn44" -- Screenshot Webhoook (WORK IN PROGRESS)
-webhooks.Log = "https://canary.discord.com/api/webhooks/920398150424997959/QaKGwhSUJ1mDWnN3cvaY2mr-MsQ0_RgUaANlr8ypoRif-tp81swaTKiyuyrvRCwdUn44" -- Main log Webhook, this webhook will log the cheaters etc...
+webhooks.StatusHook = "" -- Status Webhook (WORK IN PROGRESS)
+webhooks.ScreenShotStorage = "" -- Screenshot Webhoook (WORK IN PROGRESS)
+webhooks.Log = "" -- Main log Webhook, this webhook will log the cheaters etc...
